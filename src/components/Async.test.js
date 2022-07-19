bfb46c75afd402e7b161f0176a80553788e98668
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-07-19 16:37:36 +0800

    393. working with mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,11 @@ import Async from './Async';
 
 describe('Async Component', () => {
   test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
+
     // Arrange
     render(<Async />);
 
